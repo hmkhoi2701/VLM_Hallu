dc5b5e1a0a0283974f73b658113e06a647406657
hmkhoi2701 <hominhkhoi2701@gmail.com>
2025-10-15 11:35:08 +0000

    modify inference code log

--- generation/run_llava.py
+++ generation/run_llava.py
@@ -85,6 +85,6 @@ for line in tqdm(lines):
         )
         
     description = tokenizer.decode(outputs.sequences[0], skip_special_tokens=False)
-    with open(f"data/generated_data/test.jsonl", "w") as f:
+    with open(f"data/generated_data/test.jsonl", "a") as f:
         f.write(json.dumps({"image_id": data['image_id'], "image": data['image'], "description": description,
                             "annotations": data['annotations']}) + "\n")
